4b29cfae8165cc28c8f36659728bdced07607ba3
Blayne <bwright@scottlogic.com>
2026-06-03 09:39:29 +0100

    plugin(agent-subprocess) Remove unnecessary startup console logging
    Signed-off-by: Blayne <bwright@scottlogic.com>

--- resources/plugins/agent-subprocess-plugin/client/client.js
+++ resources/plugins/agent-subprocess-plugin/client/client.js
@@ -1,13 +1,14 @@
 'use strict';
 
-const { registerBpmnJSPlugin, registerBpmnJSModdleExtension } = require('camunda-modeler-plugin-helpers');
+const {
+  registerBpmnJSPlugin,
+  registerBpmnJSModdleExtension,
+} = require('camunda-modeler-plugin-helpers');
 
 const AgentCreateAppendProvider = require('./provider/AgentCreateAppendProvider');
-const AgentReplaceMenuProvider  = require('./provider/AgentReplaceMenuProvider');
-const AgentPropertiesProvider   = require('./provider/AgentPropertiesProvider');
-const AgentOverlayProvider      = require('./provider/AgentOverlayProvider');
-
-console.log('[Agent Subprocess Plugin] client.js loaded');
+const AgentReplaceMenuProvider = require('./provider/AgentReplaceMenuProvider');
+const AgentPropertiesProvider = require('./provider/AgentPropertiesProvider');
+const AgentOverlayProvider = require('./provider/AgentOverlayProvider');
 
 registerBpmnJSModdleExtension({
   name: 'agent',
@@ -19,26 +20,22 @@ registerBpmnJSModdleExtension({
       name: 'Config',
       superClass: ['Element'],
       properties: [
-        { name: 'provider',     isAttr: true, type: 'String' },
-        { name: 'model',        isAttr: true, type: 'String' },
-        { name: 'systemPrompt', isAttr: true, type: 'String' }
-      ]
+        { name: 'provider', isAttr: true, type: 'String' },
+        { name: 'model', isAttr: true, type: 'String' },
+        { name: 'systemPrompt', isAttr: true, type: 'String' },
+      ],
     },
     {
       name: 'Context',
       superClass: ['Element'],
-      properties: [
-        { name: 'variables', type: 'agent:Variable', isMany: true }
-      ]
+      properties: [{ name: 'variables', type: 'agent:Variable', isMany: true }],
     },
     {
       name: 'Variable',
       superClass: ['Element'],
-      properties: [
-        { name: 'name', isAttr: true, type: 'String' }
-      ]
-    }
-  ]
+      properties: [{ name: 'name', isAttr: true, type: 'String' }],
+    },
+  ],
 });
 
 registerBpmnJSPlugin({
@@ -46,12 +43,10 @@ registerBpmnJSPlugin({
     'agentCreateAppendProvider',
     'agentReplaceMenuProvider',
     'agentPropertiesProvider',
-    'agentOverlayProvider'
+    'agentOverlayProvider',
   ],
   agentCreateAppendProvider: ['type', AgentCreateAppendProvider],
-  agentReplaceMenuProvider:  ['type', AgentReplaceMenuProvider],
-  agentPropertiesProvider:   ['type', AgentPropertiesProvider],
-  agentOverlayProvider:      ['type', AgentOverlayProvider]
+  agentReplaceMenuProvider: ['type', AgentReplaceMenuProvider],
+  agentPropertiesProvider: ['type', AgentPropertiesProvider],
+  agentOverlayProvider: ['type', AgentOverlayProvider],
 });
-
-console.log('[Agent Subprocess Plugin] Plugin registered');
